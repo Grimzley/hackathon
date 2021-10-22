17f9db5ac29894578a6a1c008bda9f24620bbfc3
Ian <ireichard4@gmail.com>
2021-10-22 14:02:31 -0700

    Added gitignore and data printing

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ def main() -> None:
     df = df.drop('repo_id', 1)
     df = df.drop('repo_full_name', 1)
     print(df.head())
+    code_line = 72  # change this
+    print(f'ORIGINAL HASH : \n {df.loc[code_line]["original_commit_hash"]}')
+    print(f'POST MERGE HASH : \n {df.loc[code_line]["post_merge_hash"]}')
+    print(f'Body : \n {df.loc[code_line]["body"]}')
+    print(f'File Path : \n {df.loc[code_line]["file_path"]}')
+    print(f'Code : \n {df.loc[code_line]["code"]}')
+    print(f'Diff : \n {df.loc[code_line]["diff"]}')
 
 
 if __name__ == '__main__':
